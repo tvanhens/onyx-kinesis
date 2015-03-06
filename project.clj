64f11061433636292b775d65e2b17524b68dc340
Tyler van Hensbergen <tvanhens@gmail.com>
2015-03-06 14:29:34 -0500

    Adds project :url

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject onyx-kinesis "0.5.3"
   :description "Onyx plugin for kinesis"
-  :url "FIX ME"
+  :url "https://github.com/tvanhens/onyx-kinesis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [com.mdrogalis/onyx "0.5.3"]])
+                 [com.mdrogalis/onyx "0.5.3"]
+                 [amazonica "0.3.19"]])
